2df9ba842e152c3f46fe47f16041ff91b5bf8611
Mohan Zhang <mohzh143@student.liu.se>
2025-09-18 11:19:45 +0200

    Modify the test set

--- tests/testthat/test-euclidean.R
+++ tests/testthat/test-euclidean.R
@@ -1,7 +1,14 @@
-test_that("euclidean works on typical cases", {
-  expect_equal(euclidean(100, 1000), 100L)
-  expect_equal(euclidean(123612, 13892347912), 4L)
-  expect_equal(euclidean(0, 5), 5L)
-  expect_equal(euclidean(7, 0), 7L)
-  expect_equal(euclidean(-27, 9), 9L)
+
+
+test_that("GDC is calculated correctly.", {
+  expect_equal(euclidean(123612, 13892347912), 4)
+  expect_equal(euclidean(100, 1000), 100)
+  expect_equal(euclidean(-100, 1000), 100)
+})
+
+
+test_that("Wrong input throws an error.", {
+  expect_error(euclidean("100", 1000))
+  expect_error(euclidean(100, "1000"))
+  expect_error(euclidean(TRUE, "1000"))
 })
